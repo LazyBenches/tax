6ddcfbd476329a155fe07933fc2c035014fa15d4
lazybench <jwy226@qq.com>
2020-06-02 19:01:21 +0800

    t

--- config/config.php
+++ config/config.php
@@ -9,25 +9,27 @@
 return [
     'person' => [
         'rate' => [//增值附加税表
-            '1' => 0.07,
-            '2' => 0.03,
-            '3' => 0.02,
+            '1' => 0.07,//城市维护建设税
+            '2' => 0.03,//教育费附加
+            '3' => 0.02,//地方教育附加
+            //            '4' => 0.01,//印花税
         ],
         'statistics' => \LazyBench\Tax\Statistics\UserMonth::class,
         'taxExtReduceRate' => 0.5,//增值附加税减免比
         'basisTax' => 100000,//月税基数
         'basisTaxYear' => 1200000,//年税基数
-        'baseRate' => 1.03,
-        'addedTaxRate' => 0.03,
+        'baseRate' => 1.01,
+        'addedTaxRate' => 0.01,
     ],
     'company' => [
         'rate' => [
             '1' => 0.07,
             '2' => 0.03,
             '3' => 0.02,
+            //            '4' => 0.01,//印花税
         ],
         'taxExtReduceRate' => 0,//增值附加税减免比
-        'baseRate' => 1.03,
-        'addedTaxRate' => 0.03,
+        'baseRate' => 1.01,
+        'addedTaxRate' => 0.01,
     ],
 ];
